f976a79466a3e76fd59eff9294cae5e2c8792255
djvibez29 <djvibez29@gmail.com>
2026-02-05 15:45:59 +0100

    Update models.py

--- models.py
+++ models.py
@@ -2,25 +2,55 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+class Brand(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class EngineType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Drivetrain(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class FuelType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class VehicleType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    brand = db.Column(db.String(100))
-    custom_brand = db.Column(db.String(100))
-
-    name = db.Column(db.String(200))
-    year = db.Column(db.Integer)
-
-    body_type = db.Column(db.String(50))
-
-    mileage_miles = db.Column(db.Integer)
-
-    price_usd = db.Column(db.Integer)
-
-    transmission = db.Column(db.String(50))
-    drivetrain = db.Column(db.String(50))
-    engine = db.Column(db.String(100))
-
+    title = db.Column(db.String(200))
+    price = db.Column(db.Integer)
     description = db.Column(db.Text)
 
-    image = db.Column(db.String(300))
+    brand_id = db.Column(db.Integer, db.ForeignKey("brand.id"))
+    engine_id = db.Column(db.Integer, db.ForeignKey("engine_type.id"))
+    drivetrain_id = db.Column(db.Integer, db.ForeignKey("drivetrain.id"))
+    fuel_id = db.Column(db.Integer, db.ForeignKey("fuel_type.id"))
+    type_id = db.Column(db.Integer, db.ForeignKey("vehicle_type.id"))
+
+    brand = db.relationship("Brand")
+    engine = db.relationship("EngineType")
+    drivetrain = db.relationship("Drivetrain")
+    fuel = db.relationship("FuelType")
+    vehicle_type = db.relationship("VehicleType")
+
+    images = db.relationship("CarImage", backref="car", cascade="all,delete")
+
+
+class CarImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255))
+    car_id = db.Column(db.Integer, db.ForeignKey("car.id"))
