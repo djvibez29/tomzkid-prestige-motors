785286667ffcae530edbcd8f318857dd44be35ec
djvibez29 <djvibez29@gmail.com>
2026-02-06 05:39:01 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,40 +1,24 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from extensions import db
 
-db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-    password_hash = db.Column(db.String(200))
-    is_admin = db.Column(db.Boolean, default=True)
-
-    def set_password(self, pw):
-        self.password_hash = generate_password_hash(pw)
-
-    def check_password(self, pw):
-        return check_password_hash(self.password_hash, pw)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
 
-class Car(db.Model):
+class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150))
+
+    title = db.Column(db.String(120))
     price = db.Column(db.Integer)
-    description = db.Column(db.Text)
 
-    images = db.relationship("CarImage", backref="car", cascade="all,delete")
-    favorites = db.relationship("Favorite", backref="car", cascade="all,delete")
+    engine = db.Column(db.String(80))
+    drivetrain = db.Column(db.String(50))
+    fuel = db.Column(db.String(40))
 
+    image_main = db.Column(db.String(200))
 
-class CarImage(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(200))
-    car_id = db.Column(db.Integer, db.ForeignKey("car.id"))
-
-
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    car_id = db.Column(db.Integer, db.ForeignKey("car.id"))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
