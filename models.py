c7cdc6990fcae5923f01de88fb1022a371b2a1b4
djvibez29 <djvibez29@gmail.com>
2026-02-15 22:42:24 +0100

    Refactor User and Vehicle models, add Wishlist model

--- models.py
+++ models.py
@@ -1,46 +1,51 @@
-from extensions import db
-from flask_login import UserMixin
 from datetime import datetime
+from flask_login import UserMixin
+from extensions import db
 
 
-class User(db.Model, UserMixin):
+# ---------------- USER ----------------
 
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
-    password_hash = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default="user")  
+    plan = db.Column(db.String(20), default="free")
 
-    role = db.Column(db.String(20), default="dealer")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+
+# ---------------- VEHICLE ----------------
 
 class Vehicle(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
 
-    brand = db.Column(db.String(50))
-    model = db.Column(db.String(50))
-    trim = db.Column(db.String(50))
-
+    brand = db.Column(db.String(100))
+    model = db.Column(db.String(100))
     price = db.Column(db.Integer)
 
     year = db.Column(db.Integer)
     mileage = db.Column(db.Integer)
 
-    transmission = db.Column(db.String(20))
-    drivetrain = db.Column(db.String(20))
-    body_style = db.Column(db.String(20))
-
-    engine_type = db.Column(db.String(50))
-    engine_layout = db.Column(db.String(10))
-
-    interior_color = db.Column(db.String(30))
-    exterior_color = db.Column(db.String(30))
-
     image_url = db.Column(db.String(300))
 
     is_approved = db.Column(db.Boolean, default=False)
 
+    dealer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    dealer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+# ---------------- WISHLIST ----------------
+
+class Wishlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.id"))
+
+    vehicle = db.relationship("Vehicle")
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
