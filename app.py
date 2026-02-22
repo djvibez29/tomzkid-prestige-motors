1bbba4ac4937615ac510bc0c1acaf3a8722e5adc
djvibez29 <djvibez29@gmail.com>
2026-02-22 14:46:11 +0100

    Add main block to run Flask app

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import os
 import requests
 from werkzeug.utils import secure_filename
 
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 from flask import (
     Flask, render_template, redirect,
     request, url_for, flash
